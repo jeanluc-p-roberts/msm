b0da2f424a6dba5f0d175b3d5d00fbce0b268825
Jean-Luc Roberts <jeanluc.p.roberts@gmail.com>
2017-04-04 17:33:56 +0000

    Added the start of the WebSocket client and server code

--- bin.js
+++ bin.js
@@ -1,48 +1,14 @@
 const readline = require('readline');
 const MSMServer = require('./msm').MSMServer;
-const express = require('express');
-const http = require('http');
-const WebSocket = require('ws');
+const WebServer = require('./webserver');
+
 
 var msmserver = new MSMServer();
-msmserver.loadServerList();
+var webserver = new WebServer(msmserver);
+webserver.listen();
 
-//Set up express for static processing (everything will be done over websockets)
-var app = express();
-//app.use('/web', express.static('public'));
-app.use('/web', express.static(__dirname + "/public"));
-app.get('/', (req, res) => {
-	res.redirect('/web/');
-});
 
-//Set up websocket
-const server = http.createServer(app);
-const wss = new WebSocket.Server({server});
-
-wss.on('connection', (ws) => {
-	ws.on('message', (message) => {
-		var m = JSON.parse(message);
-		if(m.command == "exit"){
-			process.exit(0);
-		} else{
-			msmserver.executeCommand(m.command, m.args, (output, err) => {
-				var toSend = { status: null, msg: null };
-				if(err){
-					toSend.status = "err";
-					toSend.msg = err;
-				} else if(output){
-					toSend.status = "ok";
-					toSend.msg = output;
-				} else return;
-				ws.send(JSON.stringify(toSend));
-			});
-		}
-	});
-});
-server.listen(8080, () => {
-});
-
-const rl = readline.createInterface({
+/*const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   prompt: '>'
@@ -66,4 +32,4 @@ rl.on('line', (input) => {
 		});
 	}
 });
-rl.prompt();
+rl.prompt();*/
